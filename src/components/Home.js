17fce943596d8945e7097bd64f393be882c0e3ce
zacoope1 <zacoope1@asu.edu>
2021-01-05 15:57:24 -0700

    Mobile nav bar changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -53,20 +53,28 @@ import Register from './Register';
                                     this.state.menuEnabled === false ?
                                     (
                                         <div className="nav-drop-menu">
-                                            <button onClick={ () => { this.setState( { menuEnabled: true } ) } }>+</button>
+                                            <div class="container" onClick={ () => { this.setState( { menuEnabled: true } ) } }>
+                                                <div class="bar1"></div>
+                                                <div class="bar2"></div>
+                                                <div class="bar3"></div>
+                                            </div>
                                         </div>
                                     )
                                     :
                                     (
                                         <div className="nav-drop-menu">
-                                            <button onClick={ () => { this.setState( { menuEnabled: false } ) } }>-</button>
-                                            <ul>
-                                                <li><Link to="/">Home</Link></li>
-                                                <li><Link to="/login">ToDo App</Link></li>
-                                                <li><a href="https://github.com/zacoope1">Github</a></li>
-                                                <li><a href="https://www.linkedin.com/in/zachary-cooper-92b2a5159/" >LinkedIn</a></li>
-                                                <li><a href="https://zacharyacooper.com">Capstone</a></li>
-                                            </ul>
+                                            <div class="change" onClick={ () => { this.setState( { menuEnabled: false } ) } }>
+                                                <div class="bar1"></div>
+                                                <div class="bar2"></div>
+                                                <div class="bar3"></div>
+                                            </div>
+                                            <div className="nav-drop-menu-links">
+                                                <Link onClick={ () => { this.setState({ menuEnabled: false }) } } to="/">Home</Link>
+                                                <Link onClick={ () => { this.setState({ menuEnabled: false }) } } to="/login">ToDo App</Link>
+                                                <a onClick={ () => { this.setState({ menuEnabled: false }) } } href="https://github.com/zacoope1">Github</a>
+                                                <a onClick={ () => { this.setState({ menuEnabled: false }) } } href="https://www.linkedin.com/in/zachary-cooper-92b2a5159/" >LinkedIn</a>
+                                                <a onClick={ () => { this.setState({ menuEnabled: false }) } } href="https://zacharyacooper.com">Capstone</a>
+                                            </div>
                                         </div>
                                     )
                                 )
