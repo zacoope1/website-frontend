adb247239ebd5cdd64f61cb04ebf25f1e717e50e
zacoope1 <zacoope1@asu.edu>
2021-01-05 17:01:51 -0700

    Added resume.

--- src/components/Home.js
+++ src/components/Home.js
@@ -71,6 +71,7 @@ import Register from './Register';
                                             <div className="nav-drop-menu-links">
                                                 <Link onClick={ () => { this.setState({ menuEnabled: false }) } } to="/">Home</Link>
                                                 <Link onClick={ () => { this.setState({ menuEnabled: false }) } } to="/login">ToDo App</Link>
+                                                <a href="/Zachary-Cooper-Resume.pdf">Resume</a>
                                                 <a onClick={ () => { this.setState({ menuEnabled: false }) } } href="https://github.com/zacoope1">Github</a>
                                                 <a onClick={ () => { this.setState({ menuEnabled: false }) } } href="https://www.linkedin.com/in/zachary-cooper-92b2a5159/" >LinkedIn</a>
                                                 <a onClick={ () => { this.setState({ menuEnabled: false }) } } href="https://zacharyacooper.com">Capstone</a>
@@ -83,6 +84,7 @@ import Register from './Register';
                                     <ul>
                                         <li><Link to="/">Home</Link></li>
                                         <li><Link to="/login">ToDo App</Link></li>
+                                        <li><a href="/Zachary-Cooper-Resume.pdf">Resume</a></li>
                                         <li><a href="https://github.com/zacoope1">Github</a></li>
                                         <li><a href="https://www.linkedin.com/in/zachary-cooper-92b2a5159/" >LinkedIn</a></li>
                                         <li><a href="https://zacharyacooper.com">Capstone</a></li>
--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -11,9 +11,9 @@ class LandingPage extends React.Component {
         return (
             <div className="page-content-wrapper">
                 <div className="Landing-Page-Content">
-                    <h3>Computer Science Undergrad at Arizona State University</h3>
+                    <h3>Computer Science Undergraduate at Arizona State University</h3>
                     <h3>Expected graduation: May 2021</h3>
-                    <h3>This website is currently under construction. Please check back later.</h3>
+                    <h3>Please note that this website is currently under construction.</h3>
                     <h3>In the meantime, please check out my Resume, GitHub, LinkedIn, and Senior Capstone Project!</h3>
                 </div>
             </div>
