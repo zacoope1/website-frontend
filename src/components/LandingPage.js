ae593d7189dd1ce7031218296c013cfc4707ce2a
zacoope1 <zacoope1@asu.edu>
2021-02-03 16:13:45 -0700

    Added web icon. Adjusted theme.

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -6,22 +6,37 @@ class LandingPage extends React.Component {
 
     constructor() {
         super();
-        this.state = {}
+        this.state = {
+            disclaimerShown: true,
+        }
     }
 
     render(){
         return (
             <div className="page-content-wrapper">
                 <div className="landing-page-content">
+                    {
+                        this.state.disclaimerShown ? 
+                            (
+                                <div className="Disclaimer">
+                                    <p><b>Disclaimer: </b>This website is currently under development. There may be some theme errors and portfolio project bugs.</p>
+                                    <button onClick={() => {this.setState({disclaimerShown: false})}}>x</button>
+                                </div>
+                            )
+                        :
+                            null
+                    }
                     <div className="info-card">
                         <img className="Headshot" src={ME} alt=""/>
                         <div className="about-me">
                             <h1>About Me</h1>
                             <h3>Computer Science Undergraduate at Arizona State University</h3>
-                            <h3>Expected graduation: May 2021</h3>
-                            <h3>I have 8 months of fullstack development internship experience at Equinix.</h3>
-                            <h3>Please note that this website is still under development and not complete.</h3>
-                            <h3>In the meantime, please check out my Resume, GitHub, LinkedIn, and Senior Capstone Project!</h3>
+                            <h3>I am expected to graduate in May of 2021</h3>
+                            <h3>I have 8 months of fullstack development internship experience</h3>
+                            <h3>I am fluent in Java, Python, C++, Javascript, HTML, and CSS</h3>
+                            <h3>I have worked professionally with React, Redux, Spring Boot, and PostgreSQL</h3>
+                            <h3>I also have some academic experience with C#, .Net/ASP.Net, and Swift</h3>
+                            <h3>Portfolio Projects: Coming Soon!</h3>
                         </div>
                     </div>
                 </div>
